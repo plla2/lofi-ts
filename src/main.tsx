918c12677754fecd4a9b21851fadf10b7c16d0a3
plla2 <jhbljs92@naver.com>
2023-07-09 17:38:33 +0900

    RTK/ ts settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
+);
