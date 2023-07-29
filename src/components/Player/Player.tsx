44624592c254157248350a72c39e8b3bf3a743ff
plla2 <jhbljs92@naver.com>
2023-07-29 16:52:20 +0900

    Player 컴포넌트

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -4,6 +4,47 @@ import pauseBtn from "../../../public/assets/icons/pause.svg";
 import nextBtn from "../../../public/assets/icons/next.svg";
 
 const Player = () => {
+  const data = useSelector((state) => state.volume);
+
+  const { volumeValue } = data;
+
+  const audioElement = useRef();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioElement.current.play();
+    } else {
+      audioElement.current.pause();
+    }
+    audioElement.current.volume = volumeValue / 100;
+  });
+
+  const SkipSong = (forwards = true) => {
+    if (forwards) {
+      setCurrentSongIndex(() => {
+        let temp = currentSongIndex;
+        temp++;
+
+        if (temp > songs.length - 1) {
+          temp = 0;
+        }
+
+        return temp;
+      });
+    } else {
+      setCurrentSongIndex(() => {
+        let temp = currentSongIndex;
+        temp--;
+
+        if (temp < 0) {
+          temp = songs.length - 1;
+        }
+
+        return temp;
+      });
+    }
+  };
   return (
     <div className="music-player">
       <audio loop></audio>
