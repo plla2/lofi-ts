998afc781b64937d404580755ac2a104ce78e419
plla2 <jhbljs92@naver.com>
2023-07-31 19:15:31 +0900

    rainSlice 리듀서 생성

---
+++ src/store/rainSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface RainPayload {
+  rainMode: string;
+  rainValue: number;
+}
+
+const initialState: { rainMode: string; rainValue: number } = {
+  rainMode: "clear",
+  rainValue: 0,
+};
+
+const rainSlice = createSlice({
+  name: "rain",
+  initialState: {
+    rainMode: "clear",
+    rainValue: 0,
+  },
+  reducers: {
+    setRain: (state, action: PayloadAction<RainPayload>) => {
+      state.rainMode = action.payload.rainMode;
+      state.rainValue = action.payload.rainValue;
+    },
+    changeRainStatus: (
+      state,
+      action: PayloadAction<{ currentStatus: string; value: number }>
+    ) => {
+      const { currentStatus, value } = action.payload;
+
+      if (currentStatus === "rain") {
+        state.rainMode = "clear";
+      } else if (currentStatus === "clear") {
+        state.rainMode = "rain";
+      }
+
+      state.rainValue = value;
+    },
+  },
+});
+
+export const { setRain, changeRainStatus } = rainSlice.actions;
+export default rainSlice.reducer;
